7afc1fced96ae19322e7a5d03bdc80c4a48ce7f8
WonahGodwino <atamswgo1@gmail.com>
2025-06-12 16:16:59 +0100

    Updating name on User model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true }, // Full name
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', UserSchema);
+
